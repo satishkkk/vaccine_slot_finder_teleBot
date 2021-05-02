212539cbe4e032a82d905bb3ce1128552d7ff4c9
Satish Kathiriya <satishkathiriya99@gmail.com>
2021-05-03 03:22:45 +0530

    JIRA-1 : InMemory tele bot

--- main.py
+++ main.py
@@ -120,14 +120,7 @@ def main():
     dispatcher.add_handler(MessageHandler(Filters.text, get_pincode))  # if the user sends text
     dispatcher.add_error_handler(error)
 
-    # Start the Bot
-    updater.start_webhook(listen="0.0.0.0",
-                          port=int(PORT),
-                          url_path=TOKEN,
-                          webhook_url='https://vaccine-finder-bot.herokuapp.com/'+TOKEN)
-    # updater.bot.set_webhook(url=settings.WEBHOOK_URL)
-    # updater.bot.set_webhook("https://vaccine-finder-bot.herokuapp.com/ " + TOKEN)
-    # updater.start_polling()
+    updater.start_polling()
     getVaccineData()
     updater.idle()
 
